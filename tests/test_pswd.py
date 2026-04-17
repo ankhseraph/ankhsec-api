1dfd02d797ddd3add25e2154703f2e2a1e1fb8f2
ankhseraph <git@ankhseraph.com>
2026-04-17 20:29:44 +0300

    rename pass to pswd for clarity

--- tests/test_pswd.py
+++ tests/test_pswd.py
@@ -3,15 +3,15 @@ from app.main import app
 
 client = TestClient(app)
 
-def test_pass_empty():
+def test_pswd_empty():
     r = client.get("/generate-pass?useUpper=false&useLower=false&useSpecial=false&useDigits=false")
     assert r.status_code == 400
 
-def test_pass_default():
+def test_pswd_default():
     r = client.get("/generate-pass")
     assert r.status_code == 200 and len(r.json()["password"]) == 8
 
-def test_pass_long():
+def test_pswd_long():
     r = client.get("/generate-pass?length=400")
     assert r.status_code == 200 and len(r.json()["password"]) == 400
 
